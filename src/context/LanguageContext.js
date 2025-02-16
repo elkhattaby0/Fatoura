6acf31f01d29ac68ee9b908e663be96f0a1c9b10
elkhattaby0 <eelkhattaby@gmail.com>
2025-02-17 00:16:01 +0100

    17-02-2025 00:15

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -1,20 +1,17 @@
-// @ts-ignore
 "use client";
 import { createContext, useContext, useState, useEffect } from "react";
 import en from "../translation/en";
 import ar from "../translation/ar";
 
-// @ts-ignore
 const translations = { en, ar };
 
-// @ts-ignore
+
 const LanguageContext = createContext(null);
 
-// @ts-ignore
 export const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState("en");
   
-  // @ts-ignore
+  
   const changeLanguage = (lang) => {
     setLanguage(lang);
     document.documentElement.lang = lang;
@@ -26,14 +23,12 @@ export const LanguageProvider = ({ children }) => {
   }, [language]);
 
   return (
-    // @ts-ignore
     <LanguageContext.Provider value={{ language, changeLanguage, t: translations[language] }}>
       {children}
     </LanguageContext.Provider>
   );
 };
 
-// @ts-ignore
 export const useLanguage = () => {
   const context = useContext(LanguageContext);
   if (!context) throw new Error("useLanguage must be used within a LanguageProvider");
